06affa06fe2530df7de8faa9594ea021e35c25f8
Vimbayinashe <vimbayinashemandaza@gmail.com>
2020-02-19 12:13:41 +0100

    Move country selection to CountryChoice.vue

--- src/App.vue
+++ src/App.vue
@@ -4,47 +4,14 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <!-- name: {{ $route.name }}  unique identifier for this route-->
-      <div id = "display-option">
-        <label for="display-format">Location format:</label>
-        <select
-          id="display-format"
-          v-model="location"
-          @click="$store.dispatch('$store.mutations.changeDate', location)"
-          >
-          <option value="us-US">English (US)</option>
-          <option value="gb-GB">English (UK)</option>
-          <option value="de-DE">Germany</option>
-          <option value="sv-SV">Sweden</option>
-        </select>
-        <div id = "small-flag">
-          {{ location.slice(3) }}
-          <div v-if="location == 'us-US'">
-            <img src="./assets/flags/us.png" alt="">
-          </div>
-          <div v-else-if="location == 'gb-GB'">
-            <img src="./assets/flags/gb.png" alt="">
-          </div>
-          <div v-else-if="location == 'de-DE'">
-            <img src="./assets/flags/de.png" alt="">
-          </div>
-          <div v-else>
-            <img src="./assets/flags/sv.png" alt="">
-          </div>
-        </div>
-
-        <!-- <label for="display-currency">Select display:</label>
-        <select id="display-currency">
-          <option value="USD">USD</option>
-          <option value="20">20 per page</option>
-          <option value="all">View all</option>
-        </select> -->
-      </div>
+      <CountryChoice></CountryChoice>
       <div v-if="$route.name == 'Home' && currencies != null">
         <p>{{ currencies.data.coins[0].name }}</p>
         <TopCurrencies :count="5"></TopCurrencies>
 
         <!-- This prop lets me change number of top currencies displayed on Home Screen  -->
       </div>
+
     </div>
     <router-link to="/currencies">See All CryptoCurrencies</router-link>
     <router-view/>
@@ -54,11 +21,13 @@
 <script>
 
 import store from './store'
+import CountryChoice from '@/components/CountryChoice.vue'
 import TopCurrencies from '@/components/TopCurrencies.vue'
 
 export default {
   components: {
     // History,
+    CountryChoice,
     TopCurrencies
   },
   created () {
---
+++ src/components/CountryChoice.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id = "display-option">
+        <label for="display-format">Location format:</label>
+        <select
+          id="display-format"
+          v-model="location"
+          @click="$store.dispatch('$store.mutations.changeDate', location)"
+          >
+          <option value="us-US">English (US)</option>
+          <option value="gb-GB">English (UK)</option>
+          <option value="de-DE">Germany</option>
+          <option value="sv-SV">Sweden</option>
+        </select>
+        <div id = "small-flag">
+          {{ location.slice(3) }}
+          <div v-if="location == 'us-US'">
+            <img src="../assets/flags/us.png" alt="">
+          </div>
+          <div v-else-if="location == 'gb-GB'">
+            <img src="../assets/flags/gb.png" alt="">
+          </div>
+          <div v-else-if="location == 'de-DE'">
+            <img src="../assets/flags/de.png" alt="">
+          </div>
+          <div v-else>
+            <img src="../assets/flags/sv.png" alt="">
+          </div>
+        </div>
+
+        <!-- <label for="display-currency">Select display:</label>
+        <select id="display-currency">
+          <option value="USD">USD</option>
+          <option value="20">20 per page</option>
+          <option value="all">View all</option>
+        </select> -->
+
+      </div>
+</template>
+
+<script>
+
+export default {
+
+  computed: {
+    currencies () {
+      return this.$store.state.currencies
+    },
+    location: {
+      get () {
+        return this.$store.state.location
+      },
+      set (location) {
+        this.$store.commit('numberFormat', location)
+        // this.$store.commit('changeCurrenciesFormat', location)
+      }
+    }
+  },
+  name: 'CountryChoice'
+
+}
+</script>
